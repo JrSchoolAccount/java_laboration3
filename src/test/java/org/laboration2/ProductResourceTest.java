01caedb4588c924fd042f07952e7eb21e0046fb1
JrSchoolAccount <144328779+JrSchoolAccount@users.noreply.github.com>
2024-10-14 20:21:06 +0200

    Added test with mocking without success

--- src/test/java/org/laboration2/ProductResourceTest.java
+++ src/test/java/org/laboration2/ProductResourceTest.java
@@ -1,22 +1,30 @@
 package org.laboration2;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.mock.MockDispatcherFactory;
 import org.jboss.resteasy.mock.MockHttpRequest;
 import org.jboss.resteasy.mock.MockHttpResponse;
 import org.jboss.resteasy.spi.Dispatcher;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.laboration2.entities.Product;
+import org.laboration2.entities.ProductType;
 import org.laboration2.resource.ProductResource;
 import org.laboration2.service.Warehouse;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.time.LocalDate;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -37,7 +45,7 @@ public class ProductResourceTest {
     }
 
     @Test
-    void whenPostingValidProductThenShouldReturn201Created() throws URISyntaxException, UnsupportedEncodingException {
+    void whenPostingValidProductThenShouldReturn201Created() throws URISyntaxException {
         String json = """
     {
         "id": 1,
@@ -56,5 +64,60 @@ public class ProductResourceTest {
         dispatcher.invoke(request, response);
 
         assertEquals(201, response.getStatus());
+    }
+
+    @Test
+    void allProductsGivesJsonArray() throws URISyntaxException, UnsupportedEncodingException, JSONException {
+        LocalDate date = LocalDate.of(2024, 3,10);
+        System.out.println("LocalDate: " + date);
+        List<Product> mockedProducts = List.of(
+                new Product(1, "Morning star", ProductType.WEAPON, 2, date, date),
+                new Product(2, "Shiv", ProductType.WEAPON, 3, date, date),
+                new Product(3, "Broad sword", ProductType.WEAPON, 4, date, date)
+        );
+
+        System.out.println("mockedProducts: " + mockedProducts);
+
+        Mockito.when(warehouse.getAllProducts()).thenReturn(mockedProducts);
+
+        MockHttpRequest request = MockHttpRequest.get("/products");
+        MockHttpResponse response = new MockHttpResponse();
+        dispatcher.invoke(request, response);
+
+        assertEquals(200, response.getStatus());
+
+        String jsonBody = response.getContentAsString();
+        System.out.println("JSON Response: " + jsonBody);
+
+        String expectedJsonResponse = """
+            [
+                {
+                    "id": 1,
+                    "name": "Morning star",
+                    "rating": 2,
+                    "type": "WEAPON",
+                    "created": "2024-10-14",
+                    "modified": "2024-10-14"
+                },
+                {
+                    "id": 2,
+                    "name": "Shiv",
+                    "rating": 3,
+                    "type": "WEAPON",
+                    "created": "2024-10-14",
+                    "modified": "2024-10-14"
+                },
+                {
+                    "id": 3,
+                    "name": "Broad sword",
+                    "rating": 4,
+                    "type": "WEAPON",
+                    "created": "2024-10-14",
+                    "modified": "2024-10-14"
+                }
+            ]
+        """;
+
+        JSONAssert.assertEquals(expectedJsonResponse, jsonBody, JSONCompareMode.LENIENT);
     }
 }
